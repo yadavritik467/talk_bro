24de934805821609a011674b3c94643ed7c7c404
Ritik Yaduvanshi <130385697+yadavritik467@users.noreply.github.com>
2025-02-09 16:15:13 +0530

    updated env file
    in production side i was getting the error it was not working proper for backend url

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -18,7 +18,7 @@ export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const BACKEND_URL =  window.location.hostname === 'localhost'
 ? import.meta.env.VITE_API_URL
-: 'http://192.168.1.17:4500';
+: import.meta.env.VITE_API_URL;
   // import.meta.env.VITE_MODE === "dev"
   //   ? "http://192.168.1.15:4500"
   //   : import.meta.env.VITE_API_URL;
